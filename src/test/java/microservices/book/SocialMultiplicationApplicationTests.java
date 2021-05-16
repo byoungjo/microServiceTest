a093c22b8ff4ee9ee28136a9a5bee98882a69b30
byoungjo <ishtar1313!!>
2021-05-16 21:30:00 +0900

    dd

--- src/test/java/microservices/book/SocialMultiplicationApplicationTests.java
+++ src/test/java/microservices/book/SocialMultiplicationApplicationTests.java
@@ -1,7 +1,11 @@
 package microservices.book;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
